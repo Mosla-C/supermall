c68c4d5043b126b0b42e81c24fd0a50162f284c9
邓章辉 <929628481@qq.com>
2020-07-26 16:41:19 +0800

    days1

---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,15 @@
+<!--  -->
+<template>
+	<div id="profile">
+		<h2>我的</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "profile",
+}
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+</style>
